cbca56024e707783a9ea65a3fd9f3a74a5664ae0
wangmyhome <1131207997@qq.com>
2022-01-07 16:12:03 +0800

    策略模式

--- 策略模式_StrategyPattern/src/main/java/com/pattern/strategy/Men.java
+++ 策略模式_StrategyPattern/src/main/java/com/pattern/strategy/Men.java
@@ -1,7 +1,6 @@
 package com.pattern.strategy;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 /**
  * @author 王山鹏 (shanpeng.wang@transsion.com)
@@ -10,16 +9,16 @@ import java.util.Comparator;
 public class Men {
 
     public static void main(String[] args) {
-        Arrays.sort(new Integer[]{2,3,42,2}, (o1, o2) -> o2 - o1);
+//        Arrays.sort(new Integer[]{2,3,42,2}, (o1, o2) -> o2 - o1);
         GoHomeContext goHomeContext = new GoHomeContext();
         //1.开车
         goHomeContext.setGoHomeStrategy(new GoHomeByCar());
         goHomeContext.goHome();
         //2.坐火车
-//        goHomeContext.setGoHomeStrategy(new GoHomeByTrain());
-//        goHomeContext.goHome();
-//        //3.坐飞机
-//        goHomeContext.setGoHomeStrategy(new GoHomeByAirplane());
-//        goHomeContext.goHome();
+        goHomeContext.setGoHomeStrategy(new GoHomeByTrain());
+        goHomeContext.goHome();
+        //3.坐飞机
+        goHomeContext.setGoHomeStrategy(new GoHomeByAirplane());
+        goHomeContext.goHome();
     }
 }
